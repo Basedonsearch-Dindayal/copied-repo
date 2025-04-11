0a280830b2df1c1d111fbf941342367fba0c1bea
Basedonsearch-Dindayal <74675296+Basedonsearch-Dindayal@users.noreply.github.com>
2025-04-11 13:43:20 +0530

    Delete backend directory

--- backend/models/Achivements.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const achievementSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  date: { type: Date, default: Date.now, expires: 604800 }, // Auto-delete after 7 days
-});
-
-module.exports = mongoose.model("Achievement", achievementSchema);
--- backend/models/Announcement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const announcementSchema = new mongoose.Schema({
-
-  description: String,
-  date: { type: Date, default: Date.now, expires: 604800 }, // 7 days (in seconds)
-});
-
-module.exports = mongoose.model("Announcement", announcementSchema);
